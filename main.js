a0fc1c7dadf5d7abd12485dc431f148e8fabfd79
Akqqa <alexk2002@gmail.com>
2025-04-12 12:50:15 +0100

    Started drawing map

--- main.js
+++ main.js
@@ -11,11 +11,19 @@ let generator = new Generator(4, false, false, false);
 generator.generateMap();
 //generator.printMap();
 
+// Issue with this loading after the domcontentloaded and thus not showing. ugh. need promises or async or smth
+const emptyRoom = new Image();
+emptyRoom.src = "images/emptyRoom.png"
+
 document.addEventListener('DOMContentLoaded', () => { 
     const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
-    ctx.font = '30px Impact'
-    ctx.rotate(0.1)
-    ctx.fillText('Awesome!', 50, 100)
+
+    for (let x = 30; x < 420; x += 30) {
+        for (let y = 30; y < 420; y += 30) {
+            ctx.drawImage(emptyRoom, x, y, 30, 30);
+        }
+    }
+
 });
 
