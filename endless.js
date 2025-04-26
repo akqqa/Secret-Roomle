8af2254cd8b1372271a8125c67b51647e4122d0a
Akqqa <alexk2002@gmail.com>
2025-04-26 17:36:14 +0100

    made hard again HARDCORE GAMING

--- endless.js
+++ endless.js
@@ -148,7 +148,7 @@ function startGame() {
     stage = 0; // stage = 1 is room types, stage = 2 is rocks
     guesses = (
         levelnum <= 6 ? startingGuesses :
-        levelnum <= 11 ? startingGuesses + 2: // +2 bombs as can be trickier!
+        levelnum <= 11 ? startingGuesses : // changed my mind again - womb above actually easy if youre skilled enough (lol)
         levelnum === 12 ? startingGuesses + 6 : // Void is nasty
         null); // Attempt at balance based on starting floor
     secretFound = false;
