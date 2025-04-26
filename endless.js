e84fd00a327b24d0db18b17ce62b25515c7d6263
Akqqa <alexk2002@gmail.com>
2025-04-26 17:31:06 +0100

    fixed balanacing on endless

--- endless.js
+++ endless.js
@@ -148,7 +148,7 @@ function startGame() {
     stage = 0; // stage = 1 is room types, stage = 2 is rocks
     guesses = (
         levelnum <= 6 ? startingGuesses :
-        levelnum <= 11 ? startingGuesses : // +2 as harder without rooms - more dead ends for super secret
+        levelnum <= 11 ? startingGuesses + 2: // +2 bombs as can be trickier!
         levelnum === 12 ? startingGuesses + 6 : // Void is nasty
         null); // Attempt at balance based on starting floor
     secretFound = false;
