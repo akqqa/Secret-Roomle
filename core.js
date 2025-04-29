c18013da89939bb68f4d0e830512ae92d3dcf7a1
Akqqa <alexk2002@gmail.com>
2025-04-29 13:31:40 +0100

    refined to allow rotating to change canvas size

--- core.js
+++ core.js
@@ -614,8 +614,10 @@ export function runCore(gamemode) {
         }
     })
 
+    var lastWidth = window.innerWidth;
     addEventListener("resize", (event) => {
-        if (window.visualViewport?.scale !== 1) return;
+        if (window.visualViewport?.scale !== 1 && window.innerWidth) return;
+        lastWidth = window.innerWidth;
         setScaling();
         canvas.width = mapSize;
         canvas.height = mapSize;
